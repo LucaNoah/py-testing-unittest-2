3ca7367ecfbd420bd608edf5332a7892e3dae7e7
LucaNoah <luca.gfeller2@gmail.com>
2022-09-09 18:13:56 +0000

    in test_student define test_alert_santa

--- test_student.py
+++ test_student.py
@@ -1,12 +1,16 @@
 import unittest
 from student import Student
 
+
 class TestStudent(unittest.TestCase):
+    def test_full_name(self):
+        student = Student("John", "Doe")
+        self.assertEqual(student.full_name, "John Doe")
 
-  def test_full_name(self):
-    student = Student("John", "Doe")
+    def test_alert_santa(self):
+        student = Student("John",  "Doe")
+        student.alert_santa()
+        self.assertTrue(student.naughty_list)
 
-    self.assertEqual(student.full_name, "John Doe")
-
-  if __name__ == "__main__":
+if __name__ == "__main__":
     unittest.main()
